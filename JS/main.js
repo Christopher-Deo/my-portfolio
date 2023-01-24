c26f1377d2a2683cc243d7742e02f77493599bd9
Christopher-Deo <chris@christopherdeo.com>
2023-01-24 08:59:13 -0600

    completed base files

--- JS/main.js
+++ JS/main.js
@@ -102,7 +102,41 @@ $(document).ready(function () {
             }
         });
         return false;
-    })
+    });
+    //making the navbar sticky on scroll after hero section
+
+    $("#navigation li a").click(function (e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+    });
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $("body");
+
+        if ($(window).scrollTop() >= navTop) {
+            body.addClass("fixedNav");
+        }
+        else {
+            body.removeClass("fixedNav");
+        }
+
+
+
+
+    }
+
 });
+
+
 
 
